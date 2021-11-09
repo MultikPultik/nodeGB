08946b1f5dd6de1f9c239590775ea03aa20f1c77
AvisRara <tsiganov.da@gmail.com>
2021-11-09 23:19:41 +0400

    Home work 5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,101 @@
 import { useState, useEffect } from "react";
-import { Layout, List, Typography } from "antd";
+import { Layout, List } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
 
 const port = 5000;
 const server = "http://localhost:" + port;
 
+function ListFiles(props) {
+  const { isDir, link, href = "" } = props.param;
+  return (
+    <a data-isdir={isDir} href={href + "/" + link}>
+      {isDir ? `[${link}]` : link}
+    </a>
+  );
+}
+
 function App() {
   const [list, setList] = useState([]);
-  const [currentDir, setCurrentDir] = useState([""]);
-
-  const getList = (data, dir = "") =>
-    data.map((item) => {
-      return (
-        <a href={server + "/root/" + dir + item.link}>
-          {item.isDir ? `[${item.link}]` : item.link}
-        </a>
-      );
-    });
+  const [prevHref, setPrevHref] = useState("");
+  const [fileContent, setFileContent] = useState("");
 
   useEffect(() => {
     fetch(server + "/root")
       .then((res) => res.json())
       .then((data) => {
-        setList([...getList(data)]);
+        setList([...data]);
       });
   }, []);
 
   function handleListOnClick(e) {
     e.preventDefault();
-    // console.log("href - ", e.target.href);
-    const curLink = e.target.href.replace(/^.*root\//, "");
-    let fullLink = currentDir[currentDir.length - 1] + curLink;
-    console.log("currentLink - ", curLink);
-
-    if (e.target.textContent[0] === "[") {
-      //console.log("state - ", server + "/root/" + fullLink);
-      fetch(server + "/root/" + fullLink)
+    let curLink = e.target.href.replace(e.target.origin, "");
+    console.log("curLink - ", curLink);
+    if (JSON.parse(e.target.dataset.isdir)) {
+      if (curLink === "/...") {
+        curLink = prevHref;
+        curLink = curLink.replace(/\/[^\/]*$/gi, "");
+        setPrevHref(curLink);
+      } else {
+        setPrevHref(curLink);
+      }
+      fetch(server + "/root" + curLink)
         .then((res) => res.json())
         .then((data) => {
-          setList([
-            ...getList([{ isDir: false, link: "..." }, ...data], curLink + "/"),
-          ]);
+          const newData = data.map((el) => {
+            el.href = curLink;
+            return el;
+          });
+          if (curLink === "") {
+            setList([...newData]);
+          } else {
+            setList([{ isDir: true, link: "..." }, ...newData]);
+          }
         });
-      setCurrentDir([...currentDir.concat(curLink + "/")]);
     } else {
-      //console.log("getFile - ", server + "/root/" + currentDir + curLink);
-      fetch(server + "/root/" + fullLink)
+      fetch(server + "/root" + curLink)
         .then((res) => res.text())
         .then((data) => {
-          // console.log(data);
-        });
+          const cleanContent = (data) => {
+            data = data.replace(/ /gm, "\u00A0");
+            let array = data.split(new RegExp(/[\r\n]/gm));
+
+            let list = array.map((el) => {
+              return <div>{el}</div>;
+            });
+            return list;
+          };
+          setFileContent([...cleanContent(data)]);
+        })
+        .catch((er) => console.log(er));
     }
   }
 
   return (
     <>
       <Layout>
-        <Sider>
+        <Sider
+          style={{
+            height: "100vh",
+          }}
+        >
           Панель управления
           <List
-            // bordered
             dataSource={list}
             renderItem={(item) => (
-              <List.Item onClick={handleListOnClick}>{item}</List.Item>
+              <List.Item onClick={handleListOnClick}>
+                <ListFiles param={item}></ListFiles>
+              </List.Item>
             )}
           />
         </Sider>
-        <Layout>
+        <Layout
+          style={{
+            height: "100vh",
+          }}
+        >
           <Header>Файловый менеджер</Header>
-          <Content></Content>
+          <Content>{fileContent}</Content>
           <Footer>Footer</Footer>
         </Layout>
       </Layout>
